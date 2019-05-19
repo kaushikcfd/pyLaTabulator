ec1add0a8e966886dcf4123270549471ee15514c
Kaushik Kulkarni <kaushikcfd@gmail.com>
2019-05-19 14:49:51 -0500

    adds support for hierarchical row naming

--- pyLaTabulator/table.py
+++ pyLaTabulator/table.py
@@ -15,14 +15,18 @@ class Table(object):
         if all(isinstance(col_name, str) for col_name in column_names):
             column_names = [column_names]
 
+        if all(isinstance(row_name, str) for row_name in row_names):
+            row_names = [row_names]
+
         assert isinstance(data, np.ndarray)
         assert isinstance(column_names, list)
         assert isinstance(row_names, list)
         assert all(isinstance(col_name, list) for col_name in column_names)
-        assert all(isinstance(row_name, str) for row_name in row_names)
-        assert len(row_names) == data.shape[0]
+        assert all(isinstance(row_name, list) for row_name in row_names)
         assert all((data.shape[1] % len(col_name)) == 0
                 for col_name in column_names)
+        assert all((data.shape[0] % len(row_name)) == 0
+                for row_name in row_names)
 
         # }}}
 
@@ -33,23 +37,32 @@ class Table(object):
     def format(self, i, j):
         return str(self.data[i, j])
 
+    def hrule_adder(self, i):
+        return ""
+
     def preamble(self):
         return ""
 
     def generate(self, filename=None):
         formatted_data = np.ndarray(shape=self.data.shape, dtype=np.dtype('U25'))
+        rule = np.ndarray(shape=self.data.shape[0], dtype=np.dtype('U25'))
         for i in range(self.data.shape[0]):
             for j in range(self.data.shape[1]):
                 formatted_data[i, j] = self.format(i, j)
 
+            rule[i] = self.hrule_adder(i)
+
         result_template = r"""\documentclass[11pt]{article}
+\usepackage{geometry}
+\usepackage{multirow}
 \usepackage{booktabs}
+\geometry{papersize={1414mm, 1000mm}}
 ${preamble}
 
 \thispagestyle{empty}
 \begin{document}
 \begin{table}
-\begin{tabular}{c${'c'*data.shape[1]}}
+\begin{tabular}{${'c'*(data.shape[1]+len(row_names))}}
 \toprule
 % for col_name in col_names:
 <%
@@ -57,19 +70,21 @@ ${preamble}
     alignment = 'c'
     delimiter = r'} & \multicolumn{' + str(cols_per_cell) + r'}{' + alignment +r'}{'  # noqa
 %>
-\multicolumn{1}{c}{} ${delimiter[2:]}${delimiter.join(col_name)}}\\\
+\multicolumn{${len(row_names)}}{c}{} ${delimiter[2:]}${delimiter.join(col_name)}}\\\
 % endfor
 
 
 \midrule
 
 % for i in range(data.shape[0]):
-${row_names[i]}\
-% for j in range(data.shape[1]):
-  &  ${data[i, j]}\
-% endfor
-\\\
+    % for row_name in row_names:
+        <% nrows_per_row_name = data.shape[0] // len(row_name) %>
+${r'\multirow{%d}{*}{%s}' % (nrows_per_row_name,
+row_name[i//nrows_per_row_name]) if i % nrows_per_row_name == 0 else '  '}  & \
+    % endfor
+    ${'  &  '.join(elem for elem in data[i, :])}\\\
 
+${rule[i]}
 % endfor
 
 \bottomrule
@@ -79,7 +94,7 @@ ${row_names[i]}\
 """
         result = Template(result_template).render(row_names=self.row_names,
                 col_names=self.column_names, data=formatted_data,
-                preamble=self.preamble())
+                preamble=self.preamble(), rule=rule)
 
         if filename is None:
             print(result)
